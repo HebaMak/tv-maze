528d66bffa408f7eb622616155e5f32f849c9bbc
HebaSalem <puupiiiii@gmail.com>
2022-12-01 00:43:33 +0100

    create fav-icon and style it

---
+++ src/components/FavIcon.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { context } from "../hooks/context";
+
+function FavIcon({ id }) {
+  const { isFav, handleFavorite } = useContext(context);
+
+  const inFav = isFav(id);
+
+  return (
+    <div className="favorite-icon">
+      <i
+        className={inFav ? "fa fa-heart red" : "fa fa-heart"}
+        aria-hidden="true"
+        onClick={() => handleFavorite(id)}
+      ></i>
+    </div>
+  );
+}
+
+export default FavIcon;
